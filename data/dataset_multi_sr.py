27961113819a72531ffb21a227164ca3cb9b9e97
hyunbo9 <7hyll7@gmail.com>
2023-01-14 08:19:23 +0000

    after training default swinr x4

---
+++ data/dataset_multi_sr.py
@@ -0,0 +1,108 @@
+import random
+import numpy as np
+import torch.utils.data as data
+import utils.utils_image as util
+
+
+class DatasetMultiSR(data.Dataset):
+    '''
+    # -----------------------------------------
+    # Get L/H for SISR.
+    # If only "paths_H" is provided, sythesize bicubicly downsampled L on-the-fly.
+    # -----------------------------------------
+    # e.g., SRResNet
+    # -----------------------------------------
+    '''
+
+    def __init__(self, opt):
+        super(DatasetMultiSR, self).__init__()
+        self.opt = opt
+        self.n_channels = opt['n_channels'] if opt['n_channels'] else 3
+        self.sf_list = opt['scale'] if opt['scale'] else [2, 3, 4]
+        self.patch_size = self.opt['H_size'] if self.opt['H_size'] else 96
+        # ! self.L_size = self.patch_size // self.sf
+        # self.L_size = [self.patch_size / sf for sf in self.sf_list]
+
+        # ------------------------------------
+        # get paths of L/H
+        # ------------------------------------
+        self.paths_H = util.get_image_paths(opt['dataroot_H'])
+        self.paths_L = util.get_image_paths(opt['dataroot_L'])
+
+        assert self.paths_H, 'Error: H path is empty.'
+        if self.paths_L and self.paths_H:
+            assert len(self.paths_L) == len(self.paths_H), 'L/H mismatch - {}, {}.'.format(len(self.paths_L), len(self.paths_H))
+
+    def __getitem__(self, index):
+        sf = random.choice(self.sf_list)
+        L_size = self.patch_size // sf
+
+        L_path = None
+        # ------------------------------------
+        # get H image
+        # ------------------------------------
+        H_path = self.paths_H[index]
+        img_H = util.imread_uint(H_path, self.n_channels)
+        img_H = util.uint2single(img_H)
+
+        # ------------------------------------
+        # modcrop
+        # ------------------------------------
+        img_H = util.modcrop(img_H, sf)
+
+        # ------------------------------------
+        # get L image
+        # ------------------------------------
+        if self.paths_L:
+            # --------------------------------
+            # directly load L image
+            # --------------------------------
+            L_path = self.paths_L[index]
+            img_L = util.imread_uint(L_path, self.n_channels)
+            img_L = util.uint2single(img_L)
+
+        else:
+            # --------------------------------
+            # sythesize L image via matlab's bicubic
+            # --------------------------------
+            H, W = img_H.shape[:2]
+            img_L = util.imresize_np(img_H, 1 / sf, True)
+
+        # ------------------------------------
+        # if train, get L/H patch pair
+        # ------------------------------------
+        if self.opt['phase'] == 'train':
+
+            H, W, C = img_L.shape
+
+            # --------------------------------
+            # randomly crop the L patch
+            # --------------------------------
+            rnd_h = random.randint(0, max(0, H - L_size))
+            rnd_w = random.randint(0, max(0, W - L_size))
+            img_L = img_L[rnd_h:rnd_h + L_size, rnd_w:rnd_w + L_size, :]
+
+            # --------------------------------
+            # crop corresponding H patch
+            # --------------------------------
+            rnd_h_H, rnd_w_H = int(rnd_h * sf), int(rnd_w * sf)
+            img_H = img_H[rnd_h_H:rnd_h_H + self.patch_size, rnd_w_H:rnd_w_H + self.patch_size, :]
+
+            # --------------------------------
+            # augmentation - flip and/or rotate
+            # --------------------------------
+            mode = random.randint(0, 7)
+            img_L, img_H = util.augment_img(img_L, mode=mode), util.augment_img(img_H, mode=mode)
+
+        # ------------------------------------
+        # L/H pairs, HWC to CHW, numpy to tensor
+        # ------------------------------------
+        img_H, img_L = util.single2tensor3(img_H), util.single2tensor3(img_L)
+
+        if L_path is None:
+            L_path = H_path
+
+        return {'L': img_L, 'H': img_H, 'L_path': L_path, 'H_path': H_path}
+
+    def __len__(self):
+        return len(self.paths_H)
--- data/select_dataset.py
+++ data/select_dataset.py
@@ -34,6 +34,9 @@ def define_Dataset(dataset_opt):
     # -----------------------------------------
     elif dataset_type in ['sr', 'super-resolution']:
         from data.dataset_sr import DatasetSR as D
+    
+    elif dataset_type in ['multisr', 'multi-super-resolution']:
+        from data.dataset_multi_sr import DatasetMultiSR as D
 
     elif dataset_type in ['srmd']:
         from data.dataset_srmd import DatasetSRMD as D
