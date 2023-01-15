3e01dfd69b47686c2d969b6c6729b10d5ad9ca75
hyunbo9 <7hyll7@gmail.com>
2023-01-15 14:39:44 +0000

    add liif module [thanks to jinho]

--- models/network_swinir.py
+++ models/network_swinir.py
@@ -780,6 +780,12 @@ class SwinIR(nn.Module):
             # for image denoising and JPEG compression artifact reduction
             self.conv_last = nn.Conv2d(embed_dim, num_out_ch, 3, 1, 1)
 
+        ### for liif ###
+        imnet_in_dim = embed_dim
+        imnet_in_dim += 2 # attach coord
+        self.imnet = MLP(imnet_in_dim, 3, hidden_list=[256]*4)
+        ### for liif ###
+
         self.apply(self._init_weights)
 
     def _init_weights(self, m):
@@ -854,9 +860,13 @@ class SwinIR(nn.Module):
             x = self.conv_first(x)
             x = self.conv_after_body(self.forward_features(x)) + x
             x = self.conv_before_upsample(x)
-
             x = imresize.imresize(x, sizes=(target_size[-2], target_size[-1]))
             x = self.conv_last(x)
+        elif self.upsampler == 'liif':
+            x = self.conv_first(x)
+            x = self.conv_after_body(self.forward_features(x)) + x # n,c,pad_h,pad_w
+            coord = make_coord((target_size[-2], target_size[-1])).to(x.device).unsqueeze(0).expand(x.size(0),-1,-1) # n,pad_sh*pad_sw,2
+            x = self.query_rgb(x, coord).permute(0,2,1).view(x.size(0),-1,target_size[-2], target_size[-1]) # n,c=3,pad_sh,pad_sw
         else:
             # for image denoising and JPEG compression artifact reduction
             x_first = self.conv_first(x)
@@ -864,8 +874,63 @@ class SwinIR(nn.Module):
             x = x + self.conv_last(res)
 
         x = x / self.img_range + self.mean
-
         return x[:, :, :H*self.upscale, :W*self.upscale]
+
+
+    def query_rgb(self, feat, coord):
+        self.local_ensemble = True
+        if self.local_ensemble:
+            vx_lst = [-1, 1]
+            vy_lst = [-1, 1]
+            eps_shift = 1e-6
+        else:
+            vx_lst, vy_lst, eps_shift = [0], [0], 0
+
+        # field radius (global: [-1, 1])
+        rx = 2 / feat.shape[-2] / 2
+        ry = 2 / feat.shape[-1] / 2
+
+        feat_coord = make_coord(feat.shape[-2:], flatten=False).cuda() \
+            .permute(2, 0, 1) \
+            .unsqueeze(0).expand(feat.shape[0], 2, *feat.shape[-2:]) # n,2,h,w
+
+        preds = []
+        areas = []
+        for vx in vx_lst:
+            for vy in vy_lst:
+                coord_ = coord.clone()
+                coord_[:, :, 0] += vx * rx + eps_shift
+                coord_[:, :, 1] += vy * ry + eps_shift
+                coord_.clamp_(-1 + 1e-6, 1 - 1e-6)
+                q_feat = F.grid_sample(
+                    feat, coord_.flip(-1).unsqueeze(1),
+                    mode='nearest', align_corners=False)[:, :, 0, :] \
+                    .permute(0, 2, 1)
+                q_coord = F.grid_sample(
+                    feat_coord, coord_.flip(-1).unsqueeze(1),
+                    mode='nearest', align_corners=False)[:, :, 0, :] \
+                    .permute(0, 2, 1)
+                rel_coord = coord - q_coord
+                rel_coord[:, :, 0] *= feat.shape[-2]
+                rel_coord[:, :, 1] *= feat.shape[-1]
+                inp = torch.cat([q_feat, rel_coord], dim=-1)
+
+                bs, q = coord.shape[:2]
+                pred = self.imnet(inp.view(bs * q, -1)).view(bs, q, -1)
+                preds.append(pred)
+
+                area = torch.abs(rel_coord[:, :, 0] * rel_coord[:, :, 1])
+                areas.append(area + 1e-9)
+
+        tot_area = torch.stack(areas).sum(dim=0)
+        if self.local_ensemble:
+            t = areas[0]; areas[0] = areas[3]; areas[3] = t
+            t = areas[1]; areas[1] = areas[2]; areas[2] = t
+        ret = 0
+        for pred, area in zip(preds, areas):
+            ret = ret + pred * (area / tot_area).unsqueeze(-1)
+        return ret
+
 
     def flops(self):
         flops = 0
@@ -877,6 +942,44 @@ class SwinIR(nn.Module):
         flops += H * W * 3 * self.embed_dim * self.embed_dim
         flops += self.upsample.flops()
         return flops
+
+
+class MLP(nn.Module):
+
+    def __init__(self, in_dim, out_dim, hidden_list):
+        super().__init__()
+        layers = []
+        lastv = in_dim
+        for hidden in hidden_list:
+            layers.append(nn.Linear(lastv, hidden))
+            layers.append(nn.ReLU())
+            lastv = hidden
+        layers.append(nn.Linear(lastv, out_dim))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        shape = x.shape[:-1]
+        x = self.layers(x.view(-1, x.shape[-1]))
+        return x.view(*shape, -1)
+
+
+def make_coord(shape, ranges=None, flatten=True):
+    """ Make coordinates at grid centers.
+    """
+    coord_seqs = []
+    for i, n in enumerate(shape):
+        if ranges is None:
+            v0, v1 = -1, 1
+        else:
+            v0, v1 = ranges[i]
+        r = (v1 - v0) / (2 * n)
+        seq = v0 + r + (2 * r) * torch.arange(n).float()
+        coord_seqs.append(seq)
+    ret = torch.stack(torch.meshgrid(*coord_seqs), dim=-1)
+    if flatten:
+        ret = ret.view(-1, ret.shape[-1])
+    return ret
+
 
 
 if __name__ == '__main__':
